5e6e432539048f5b65751e58b0ab0087846a252d
songyongqin12580 <songyognqin12580@163.com>
2019-11-04 18:04:34 +0800

    信息路由完善

--- routes/index.js
+++ routes/index.js
@@ -1,29 +1,64 @@
 var express = require('express');
 const md5 = require('blueimp-md5')
 
-const {UserModel} = this.require('../db/models')
+const {UserModel} = require('../db/models.js')
+const filter = {password:0}
 var router = express.Router();
 
 //注册
 router.post('/register', (req, res) => {
   //获取请求参数
-  const {username,password,type} = req.query
+  const {username,password,type} = req.body
   //处理
   UserModel.findOne({username},(error,user) => {
     if(user) {
       res.send({code:1, msg: '此用户已存在'})
     }else{
       new UserModel({username, password:md5(password), type}).save((error,user) => {
-        res.cookie({userid:user._id,max,maxAge:1000*60*60*24})
+        res.cookie('userid', user._id, {maxAge: 1000*60*60*24}) //引号
         const data = {username, type, _id: user._id}
         res.send({code:0,data})
       })
     }
   })
-  
   //返回响应数据
 });
 
 //登录
+router.post('/login', (req, res) => {
+  //获取请求参数
+  const {username,password} = req.body
+  //处理
+  UserModel.findOne({username,password:md5(password)},filter,(error,user) => {
+    if(!user) {
+      res.send({code:1, msg: '用户名或者密码错误'})
+    }else{
+      res.send({code:0,data:user})
+    }
+  })
+});
+
+//修改个人信息
+router.post('/update',(req, res) => {
+  //先判断cookie中是否有userid
+  const userid = req.cookies.userid
+  if(!userid) {
+    return res.send({code:1,msg:'请先登录'})
+  }
+  const user = req.body
+  //使用userid去数据库中修改
+  UserModel.findByIdAndUpdate({_id:userid},user,(error,oldUser) => {
+    if(!oldUser) {
+      //如果没有查询到user，即前端cookie失效了
+      res.clearCookie(userid)
+      return res.send({code:1,msg:'请先登录'})
+    }
+    //查询到了，我们需要合并一个新对象返回,但不希望返回oldUser中的password
+    const {_id, username, type} = oldUser
+    const data = Object.assign(user,{_id, username, type})
+    res.send({code:0,data})
+  })
+})
+
 
 module.exports = router;
